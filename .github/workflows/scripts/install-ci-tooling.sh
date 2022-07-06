6dbfaecd81db59e0c0a5467bb45816713a386d8e
Ivan Kirchev <Ivan.Kirchev@bosch.com>
2022-07-06 14:56:58 +0300

    try with ci tooling addon
    Signed-off-by: Ivan Kirchev <Ivan.Kirchev@bosch.com>

---
+++ .github/workflows/scripts/install-ci-tooling.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# To prevent interactive shells
+export ENV DEBIAN_FRONTEND=noninteractive
+
+# Install apt & root utils needed inside devcontainer
+
+apt-get -qqy update && \
+apt-get -qqy install --no-install-recommends apt-utils software-properties-common sudo curl
+
+# Install dev utilities
+apt-get -qqy install git doxygen can-utils
+
+# Install build tools
+apt-get -qqy install \
+	cmake \
+	make
+
+# compilers
+#   compiler version must be sync with conan build profile
+apt-get -qqy install \
+  g++ \
+  g++-aarch64-linux-gnu
+
+apt-get -qqy install \
+  lcov \
+  gcovr \
+  clang-format \
+  cppcheck \
+  valgrind 
+
+# Install PIP
+apt-get -qqy install --fix-missing python3-pip
+
+# conan: dependency management
+# conan needed > 1.43 for gtest
+# cantools: code generation from .dbc file
+pip3 install \
+  'conan==1.48.1' \
+  'cantools==37.0.1'
+  
+# install docker
+# curl -fsSL https://get.docker.com -o - | bash -
+# docker --version
