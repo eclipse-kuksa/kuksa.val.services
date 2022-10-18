a3e1ea8f9fa8a3f9bc690a90c7ab755fe1299eca
vasilvas99 <ivanovvasil99@gmail.com>
2022-10-18 15:54:09 +0300

    Removed general excep handler as exceptions are handled by retry-lib

--- carsim_service/bicycle_runner.py
+++ carsim_service/bicycle_runner.py
@@ -9,7 +9,7 @@ from aioretry import (
     retry,
     # Tuple[bool, Union[int, float]]
     RetryPolicyStrategy,
-    RetryInfo
+    RetryInfo,
 )
 
 from gen_proto.sdv.databroker.v1.types_pb2 import Datapoint
@@ -44,14 +44,12 @@ rootlogger.setLevel(os.getenv("LOG_LEVEL", "CRITICAL"))
 grpclogger = logging.getLogger("grpc")
 grpclogger.setLevel(os.getenv("LOG_LEVEL", "CRITICAL"))
 
-def general_exception_handler(loop, context):
-    logger.warning("General exception: %s", context['message'])
-    return
 
 # This example shows the usage with python typings
 def _retry_policy(info: RetryInfo) -> RetryPolicyStrategy:
-    #return False, (info.fails - 1) % 3 * 0.1
+    # return False, (info.fails - 1) % 3 * 0.1
     return False, (info.fails - 1) % 3 * 2
+
 
 async def _before_retry(info: RetryInfo) -> None:
     if isinstance(info.exception, (KeyError, ValueError, KeyboardInterrupt)):
@@ -61,12 +59,16 @@ async def _before_retry(info: RetryInfo) -> None:
         # Do something
         logger.warning("Retry grpc... %s, %s", info.fails, info.exception.code())
     else:
-        logger.warning("Retry... %s, Detailed exception: %s", info.fails, info.exception)
-        
+        logger.warning(
+            "Retry... %s, Detailed exception: %s", info.fails, info.exception
+        )
+
+
 async def setup_helper() -> Databroker:
     logger.info("Using DATABROKER_ADDRESS={}".format(DATABROKER_ADDRESS))
     helper = Databroker(DATABROKER_ADDRESS)
     return helper
+
 
 # TODO: Actually, we (carsim app) are not allowed to set these datapoints, as they will be registerd
 # by the driver app. Remove the flush_controls()
@@ -77,51 +79,55 @@ async def flush_controls(helper):
     await asyncio.create_task(helper.set_uint32_datapoint(DP_STEER_ANGLE, 0))
     logger.debug(f"flushed {DP_BRAKE_POS} {DP_ACCELR_POS} {DP_STEER_ANGLE}")
 
+
 @retry(retry_policy=_retry_policy, before_retry=_before_retry)
 async def mainloop(car_sim, helper, set_model_param):
-        controls_update = asyncio.create_task(
-            helper.subscribe_datapoints(
-                KUKSA_QUERY, timeout=SIM_SPEED, sub_callback=set_model_param
-            )
+    controls_update = asyncio.create_task(
+        helper.subscribe_datapoints(
+            KUKSA_QUERY, timeout=SIM_SPEED, sub_callback=set_model_param
         )
-        await controls_update
-        car_sim.update_car()
+    )
+    await controls_update
+    car_sim.update_car()
 
-        pub_speed = asyncio.create_task(
-            helper.set_float_datapoint(
-                DP_SPEED, car_sim.speed * 3.6
-            )  # 1 m/s = 3.6 km/h
-        )
-        pub_accel_long = asyncio.create_task(
-            helper.set_float_datapoint(DP_ACCEL_LONG, car_sim.acceleration[0])  # m/s^2
-        )
-        pub_accel_lat = asyncio.create_task(
-            helper.set_float_datapoint(DP_ACCEL_LAT, car_sim.acceleration[1])  # m/s^2
-        )
-        pub_accel_vert = asyncio.create_task(
-            helper.set_float_datapoint(DP_ACCEL_LONG, 0.0)  # m/s^2
-        )
+    pub_speed = asyncio.create_task(
+        helper.set_float_datapoint(DP_SPEED, car_sim.speed * 3.6)  # 1 m/s = 3.6 km/h
+    )
+    pub_accel_long = asyncio.create_task(
+        helper.set_float_datapoint(DP_ACCEL_LONG, car_sim.acceleration[0])  # m/s^2
+    )
+    pub_accel_lat = asyncio.create_task(
+        helper.set_float_datapoint(DP_ACCEL_LAT, car_sim.acceleration[1])  # m/s^2
+    )
+    pub_accel_vert = asyncio.create_task(
+        helper.set_float_datapoint(DP_ACCEL_LONG, 0.0)  # m/s^2
+    )
 
-        await pub_speed
-        await pub_accel_lat
-        await pub_accel_long
-        await pub_accel_vert
-        logger.debug(f"Car State: {car_sim.__dict__}")
-        logger.info(f"Time={car_sim.simul_time} Speed={car_sim.speed * 3.6} km/h Acceleration (Long)={car_sim.acceleration[0]} m/s^2 Acceleration (Lat)={car_sim.acceleration[1]} m/s^2")
-        
-        #max_acceleration=6.0,   # m/s^2
-        #max_deceleration=10.0,  # m/s^2
-        #max_speed=60.0,         # m/s
+    await pub_speed
+    await pub_accel_lat
+    await pub_accel_long
+    await pub_accel_vert
 
-def register_signal_handlers(databroker: Databroker):
-    LOOP = asyncio.get_running_loop()
-    databroker.close()
-    
+    logger.debug(f"Car State: {car_sim.__dict__}")
+    logger.info(
+        f"Time={car_sim.simul_time}\
+            Speed={car_sim.speed * 3.6} km/h\
+            Acceleration (Long)={car_sim.acceleration[0]} m/s^2\
+            Acceleration (Lat)={car_sim.acceleration[1]} m/s^2"
+    )
+
+    # max_acceleration=6.0,   # m/s^2
+    # max_deceleration=10.0,  # m/s^2
+    # max_speed=60.0,         # m/s
+
+
 async def main():
     car_sim = SimulatedCar()
+
     with await setup_helper() as helper:
+
         def set_model_param(name: str, dp_raw: Datapoint) -> None:
-            logger.debug(f'{name=}, {dp_raw=}')
+            logger.debug(f"{name=}, {dp_raw=}")
             dp = helper.datapoint_to_dict(name, dp_raw)
 
             if dp["type"] == "failure_value":
@@ -138,19 +144,15 @@ async def main():
             if name == DP_STEER_ANGLE:
                 car_sim.steer_angle = value * math.pi / 180  # deg too rad
                 return
-        
+
         await flush_controls(helper)
-        
-        
         # the databroker should probably be flushed to avoid updates with stale data
         while True:
             await mainloop(car_sim, helper, set_model_param)
-        
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     LOOP = asyncio.get_event_loop()
-    LOOP.set_exception_handler(general_exception_handler)
     LOOP.run_until_complete(main())
     LOOP.close()
- 
