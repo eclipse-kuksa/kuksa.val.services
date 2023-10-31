86802f99fe9d80d22149979b75a9643238617a60
Ivan Kirchev <Ivan.Kirchev@bosch.com>
2023-10-31 17:41:22 +0200

    Main README updated with VSS 4.0 status
    - some megalinter suggestions applied
    - fixed pip3 install usage
    
    Signed-off-by: Ivan Kirchev <Ivan.Kirchev@bosch.com>

--- .github/workflows/scripts/install-ci-tooling.sh
+++ .github/workflows/scripts/install-ci-tooling.sh
@@ -20,20 +20,20 @@ sudo apt-get -qqy update &&
 	sudo apt-get -qqy install --no-install-recommends apt-utils software-properties-common sudo curl
 
 # Install dev utilities
-sudo apt-get -qqy install git doxygen can-utils python3
+sudo apt-get -qqy install --no-install-recommends git doxygen can-utils python3
 
 # Install build tools
-sudo apt-get -qqy install \
+sudo apt-get -qqy install --no-install-recommends \
 	cmake \
 	make
 
 # compilers
 #   compiler version must be sync with conan build profile
-sudo apt-get -qqy install \
+sudo apt-get -qqy install --no-install-recommends \
 	g++ \
 	g++-aarch64-linux-gnu
 
-sudo apt-get -qqy install \
+sudo apt-get -qqy install --no-install-recommends \
 	lcov \
 	gcovr \
 	clang-format \
@@ -41,7 +41,7 @@ sudo apt-get -qqy install \
 	valgrind
 
 # Install PIP
-[ -z "$(which pip3)" ] && sudo apt-get -qqy install --fix-missing python3-pip
+[ -z "$(which pip3)" ] && sudo apt-get -qqy install --no-install-recommends --fix-missing python3-pip
 
 # conan: dependency management
 # - conan needed > 1.43 for gtest
